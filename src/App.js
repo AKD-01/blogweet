03d0b5a024b6bd55e7b0534cc9798f3e63b29659
Anjali <83454075+AKD-01@users.noreply.github.com>
2023-01-02 11:56:29 +0530

    Update App.js

--- src/App.js
+++ src/App.js
@@ -1,24 +1,43 @@
-import './App.css';
-import { BrowserRouter as Router, Routes, Route, Link } from  "react-router-dom";
+import "./App.css";
+import { useState } from "react";
+import { BrowserRouter as Router, Routes, Route, Link } from "react-router-dom";
+import { signOut } from "firebase/auth";
+import { auth } from "./firebase-config";
 import Home from "./pages/Home";
 import Login from "./pages/Login";
 import CreatePost from "./pages/CreatePost";
 
 function App() {
+  const [isAuth, setIsAuth] = useState(false);
+
+  const signUserOut = () => {
+    signOut(auth).then(() => {
+      localStorage.clear();
+      setIsAuth(false);
+      window.location.pathname = "/login";
+    });
+  };
+
   return (
     <Router>
       <nav>
         <Link to="/"> Home </Link>
         <Link to="/createpost"> Create Post </Link>
-        <Link to="/login"> Login </Link>
+        {isAuth ? (
+          <button onClick={signUserOut}> Log Out</button>
+        ) : (
+          <Link to="/login"> Login </Link>
+        )}
       </nav>
       <Routes>
         <Route path="/" element={<Home />} />
         <Route path="/createpost" element={<CreatePost />} />
-        <Route path="/login" element={<Login />} />
+        <Route path="/login" element={<Login setIsAuth={setIsAuth} />} />
       </Routes>
     </Router>
   );
 }
 
 export default App;
+
+// line 20: passing state as prop in login component.
