03d73b1a10980942ecc7b5cd332d5c5573449e9f
Anjali <83454075+AKD-01@users.noreply.github.com>
2023-01-02 11:56:38 +0530

    Update Login.js

--- src/pages/Login.js
+++ src/pages/Login.js
@@ -1,11 +1,27 @@
 import React from "react";
+import { auth, provider } from "../firebase-config";
+import { signInWithPopup } from "firebase/auth";
+import { useNavigate } from "react-router-dom";
 
-function Login() {
-    return (
-        <div> 
-            Login
-        </div>
-    );
+function Login({ setIsAuth }) {
+  let navigate = useNavigate();
+
+  const signInWithGoogle = () => {
+    signInWithPopup(auth, provider).then((result) => {
+      localStorage.setItem("isAuth", true);
+      setIsAuth(true);
+      navigate("/");
+    });
+  };
+
+  return (
+    <div className="loginPage">
+      <p>Sign in with Google to continue</p>
+      <button className="login-with-google-btn" onClick={signInWithGoogle}>
+        Sign in with Google
+      </button>
+    </div>
+  );
 }
 
 export default Login;
