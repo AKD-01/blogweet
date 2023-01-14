a0833da64e932f5a7a4b2813b1c0fd0d6ccea6b1
Ritik Kumar <srivastavaritik360@gmail.com>
2023-01-14 16:11:31 +0530

    made a new page for blogs

--- src/pages/Blogpost.js
+++ src/pages/Blogpost.js
@@ -1,8 +1,28 @@
 import React from 'react'
+import { useLocation } from 'react-router-dom'
+import './pages.css'
 
 const Blogpost = () => {
+  // const post = useParams();
+  // console.log(post.blogname)
+  // const postsCollectionRef = collection(db, "posts");
+  // console.log(postsCollectionRef)
+  const location = useLocation();
+  console.log(location.state)
+  const post = location.state;
   return (
-    <div>Hello</div>
+    <div className='blogpage'>
+      <div className='blogtitle'>{post.title}</div>
+      <hr/>
+      <div className='blogcredits'>
+        By - {post.author.name}
+      </div>
+      {post.date !=null && <div>{post.date}</div>}
+      <hr/>
+      <div>
+        <img src={post.image} alt={post.title} />
+      </div>
+      </div>
   )
 }
 
--- src/pages/Home.js
+++ src/pages/Home.js
@@ -2,12 +2,12 @@ import React, { useEffect, useState } from "react";
 import { getDocs, collection, deleteDoc, doc } from "firebase/firestore";
 import { auth, db } from "../firebase-config";
 import "./Home.css";
-import { Link } from "react-router-dom";
+import { Link, useNavigate } from "react-router-dom";
 
 function Home({ isAuth }) {
   const [postLists, setPostList] = useState([]);
   const postsCollectionRef = collection(db, "posts");
-
+  console.log(auth.toJSON());
   const deletePost = async (id) => {
     const postDoc = doc(db, "posts", id);
     await deleteDoc(postDoc);
@@ -19,8 +19,9 @@ function Home({ isAuth }) {
     author: { name: "Dummy Author", id: Math.random() },
     key: Math.random(),
     postText: "Hi how are you dummy man",
+    image: "https://avatars.githubusercontent.com/in/8329?s=80&v=4",
   };
-postLists.push(DUMMY_POST);
+  postLists.push(DUMMY_POST);
   useEffect(() => {
     const getPosts = async () => {
       const data = await getDocs(postsCollectionRef);
@@ -30,33 +31,77 @@ postLists.push(DUMMY_POST);
     getPosts();
     console.log(11);
   }, []);
-
+  const navigate = useNavigate();
   return (
     <div className="homePage">
       {postLists.map((post) => {
+        console.log(post);
         return (
           <div className="post" key={post.id}>
             <div className="postHeader">
-              <div className="title">
-                <h1> {post.title}</h1>
+              <div>
+                {/* <Link
+                  to={{
+                    pathname : `/${post.author.name.replaceAll(" ", "-")}/${post.id}`,
+                    state:[{id:post.id, title:post.title}]
+                  }} 
+                > */}
+                <h1
+                  className="title"
+                  onClick={() => {
+                    navigate(
+                      `/${post.author.name.replaceAll(" ", "-")}/${post.id}`,
+                      { state: post }
+                    );
+                  }}
+                >
+                  {" "}
+                  {post.title}
+                </h1>
+                {/* </Link> */}
               </div>
               <div className="deletePost">
-                {isAuth && auth.currentUser!=null && post.author.id === auth.currentUser.uid && (
-                  <button
-                    onClick={() => {
-                      deletePost(post.id);
-                    }}
-                  >
-                    {" "}
-                    &#128465;
-                  </button>
-                )}
+                {isAuth &&
+                  auth.currentUser != null &&
+                  post.author.id === auth.currentUser.uid && (
+                    <button
+                      onClick={() => {
+                        deletePost(post.id);
+                      }}
+                    >
+                      {" "}
+                      &#128465;
+                    </button>
+                  )}
               </div>
             </div>
-            <div className="postTextContainer"> {post.postText} </div>
-            <Link to={`/${post.author.name.replaceAll(" ", "-")}`}>
-              <h3>-{post.author.name}</h3>
-            </Link>
+            <div className="contents">
+              <div className="imageCont">
+                <img src={post.image} alt={post.title} />
+              </div>
+              <div className="postTextContainer">
+                {" "}
+                {post.postText.substr(0, Math.min(post.postText.length, 200))}
+                &nbsp;&nbsp;{" "}
+                <div
+                  style={{
+                    textAlign: "right",
+                    color: "#3a363d",
+                    fontSize: ".9rem",
+                    cursor: "pointer",
+                  }}
+                  onClick={() => {
+                    navigate(
+                      `/${post.author.name.replaceAll(" ", "-")}/${post.id}`,
+                      { state: post }
+                    );
+                  }}
+                >
+                  ......Read More
+                </div>
+              </div>
+            </div>
+            <h3>-{post.author.name}</h3>
             {/* <h3>-{post.author.name}</h3> */}
           </div>
         );
