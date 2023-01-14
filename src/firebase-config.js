a40a8905858f3c8bc8e93a88f864bf56910ebb6b
Ritik Kumar <srivastavaritik360@gmail.com>
2023-01-14 12:15:15 +0530

    .

--- src/firebase-config.js
+++ src/firebase-config.js
@@ -14,12 +14,25 @@ const firebaseConfig = {
   // storageBucket: "blogweet-c28a4.appspot.com",
   // messagingSenderId: "801793825021",
   // appId: "1:801793825021:web:8b905f78556131e4c94694"
-  apiKey: "AIzaSyD9tMXwqU7ijIKnQu0vsX1UoR9FM_w7r2E",
-  authDomain: "blog-5814a.firebaseapp.com",
-  projectId: "blog-5814a",
-  storageBucket: "blog-5814a.appspot.com",
-  messagingSenderId: "787500061298",
-  appId: "1:787500061298:web:242f526d6800f0bbbf68eb",
+
+  // apiKey: "AIzaSyD9tMXwqU7ijIKnQu0vsX1UoR9FM_w7r2E",
+  // authDomain: "blog-5814a.firebaseapp.com",
+  // projectId: "blog-5814a",
+  // storageBucket: "blog-5814a.appspot.com",
+  // messagingSenderId: "787500061298",
+  // appId: "1:787500061298:web:242f526d6800f0bbbf68eb",
+
+  apiKey: "AIzaSyC5bxhpw-DzJF-1dtVQsdA5u7AaRUxQ-7g",
+
+  authDomain: "blog-divine.firebaseapp.com",
+
+  projectId: "blog-divine",
+
+  storageBucket: "blog-divine.appspot.com",
+
+  messagingSenderId: "610968567774",
+
+  appId: "1:610968567774:web:84d7f5a01962489992a74f",
 };
 
 // Initialize Firebase
