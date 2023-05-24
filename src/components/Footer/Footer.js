a09e3ab8fbf36d5807373c10e2512d8cdb9856cb
Kriti <kriti.srivastava002@gmail.com>
2023-05-24 09:27:36 +0530

    Changes proposed by admin done!

--- src/components/Footer/Footer.js
+++ src/components/Footer/Footer.js
@@ -7,6 +7,7 @@ const Footer = () => {
   return (
     <footer className='footer-text'>
     <p>Copyright  &copy; {new Date().getFullYear()}.<Link to="/"> BlogWeet</Link>. All rights reserved.</p>
+      <a href="https://github.com/AKD-01/blogweet" className=''><FaGithub className='social-icon'/></a> <FaArrowLeft/>  Follow me on Github for more exciting projects!
     </footer>
 
   )
