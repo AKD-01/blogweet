36f97d71aa3e388eaebc07134bae75761be2c71e
Vilas Chavhan <50195101+villAsh@users.noreply.github.com>
2023-05-24 11:31:20 +0530

    issue #159
    Conflicts are resolved

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -1,12 +1,14 @@
 /* eslint-disable react-hooks/exhaustive-deps */
 import React, { useEffect, useState } from "react";
 import { getDocs, collection, deleteDoc, doc } from "firebase/firestore";
-import { auth, db } from "../firebase-config";
+import { auth, db } from "../utils/firebase";
 import "./Home.css";
 import { useNavigate } from "react-router-dom";
 import { ToastContainer, toast } from "react-toastify";
 import "react-toastify/dist/ReactToastify.css";
 import Shimmer from "../components/Shimmer/Shimmer";
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import { faArrowUp } from "@fortawesome/free-solid-svg-icons";
 
 function Home({ isAuth }) {
   const [postLists, setPostList] = useState([]);
@@ -40,133 +42,168 @@ function Home({ isAuth }) {
     navigator.clipboard.writeText(`https://blogweet.vercel.app${s}`);
     toast(`Your link has been pasted to your Clipboard. Enjoy!`);
   };
+
+  const [showScrollToTop, setShowScrollToTop] = useState(false);
+  useEffect(() => {
+    const handleScroll = () => {
+      if (window.scrollY > 20) {
+        setShowScrollToTop(true);
+      } else {
+        setShowScrollToTop(false);
+      }
+    }
+    window.addEventListener("scroll", handleScroll);
+
+    return () => {
+      window.removeEventListener("scroll", handleScroll);
+    };
+    })
   return (
-    <div className="homePage">
-      {
-        postLists.length > 0 ? (
-          
-            postLists?.map((post) => {
+    <>
+      <div className="homePage">
+        {postLists.length > 0 ? (
+          postLists?.map((post) => {
             // console.log(post);
             return (
-              <div className="post" key={post.id}>
-                <div className="postHeader">
-                  <div>
-                    <h1
-                      className="title"
-                      onClick={() => {
-                        navigate(
-                          `/${post.author.name.replaceAll(" ", "-")}/${post.id}`,
-                          { state: post }
-                        );
-                      }}
-                    >
-                      {" "}
-                      {post.title}
-                    </h1>
-                    {/* </Link> */}
-                  </div>
-    
-                  <div className="deletePost">
-                    {isAuth &&
-                      auth.currentUser != null &&
-                      post.author.id === auth.currentUser.uid && (
-                        <button
-                          onClick={() => {
-                            deletePost(post.id);
-                          }}
-                        >
-                          {" "}
-                          <i
-                            className="bx bxs-message-square-x"
-                            style={{ color: "#600505" }}
-                          ></i>
-                        </button>
-                      )}
-                    <button
-                      onClick={() =>
-                        sharingHandler(
-                          `/${post.author.name.replaceAll(" ", "-")}/${post.id}`
-                        )
-                      }
-                    >
-                      <i
-                        className="bx bxs-share-alt"
-                        style={{
-                          color: "rgb(255, 255, 255)",
-                          boxShadow: " 1px 1px 1rem black",
-                          borderRadius: "1rem",
-                          background: " black",
+              <>
+                <div className="post" key={post.id}>
+                  <div className="postHeader">
+                    <div>
+                      <h1
+                        className="title"
+                        onClick={() => {
+                          navigate(
+                            `/${post.author.name.replaceAll(" ", "-")}/${
+                              post.id
+                            }`,
+                            { state: post }
+                          );
                         }}
-                      ></i>
-                    </button>
-                  </div>
-                </div>
-                <div className="contents">
-                  <div className="imageCont">
-                    <img src={post.image} alt={post.title} />
-                  </div>
-                  <div className="postTextContainer">
-                    <div style={{ height: "70px", overflow: "hidden" }}>
-                      {post.postText.substr(0, Math.min(post.postText.length, 200))}
-                      &nbsp;&nbsp;{" "}
+                      >
+                        {" "}
+                        {post.title}
+                      </h1>
+                      {/* </Link> */}
                     </div>
+
+                    <div className="deletePost">
+                      {isAuth &&
+                        auth.currentUser != null &&
+                        post.author.id === auth.currentUser.uid && (
+                          <button
+                            onClick={() => {
+                              deletePost(post.id);
+                            }}
+                          >
+                            {" "}
+                            <i
+                              className="bx bxs-message-square-x"
+                              style={{ color: "#600505" }}
+                            ></i>
+                          </button>
+                        )}
+                      <button
+                        onClick={() =>
+                          sharingHandler(
+                            `/${post.author.name.replaceAll(" ", "-")}/${
+                              post.id
+                            }`
+                          )
+                        }
+                      >
+                        <i
+                          className="bx bxs-share-alt"
+                          style={{
+                            color: "rgb(255, 255, 255)",
+                            boxShadow: " 1px 1px 1rem black",
+                            borderRadius: "1rem",
+                            background: " black",
+                          }}
+                        ></i>
+                      </button>
+                    </div>
+                  </div>
+                  <div className="contents">
+                    <div className="imageCont">
+                      <img src={post.image} alt={post.title} />
+                    </div>
+                    <div className="postTextContainer">
+                      <div style={{ height: "70px", overflow: "hidden" }}>
+                        {post.postText.substr(
+                          0,
+                          Math.min(post.postText.length, 200)
+                        )}
+                        &nbsp;&nbsp;{" "}
+                      </div>
+                      <div
+                        style={{
+                          textAlign: "right",
+                          color: "#3a363d",
+                          fontSize: ".9rem",
+                          cursor: "pointer",
+                        }}
+                        onClick={() => {
+                          navigate(
+                            `/${post.author.name.replaceAll(" ", "-")}/${
+                              post.id
+                            }`,
+                            { state: post }
+                          );
+                        }}
+                      >
+                        ......Read More
+                      </div>
+                    </div>
+                  </div>
+                  <h3>
+                    <div>📅{post.date}</div>
                     <div
-                      style={{
-                        textAlign: "right",
-                        color: "#3a363d",
-                        fontSize: ".9rem",
-                        cursor: "pointer",
-                      }}
+                      style={{ cursor: "pointer" }}
                       onClick={() => {
-                        navigate(
-                          `/${post.author.name.replaceAll(" ", "-")}/${post.id}`,
-                          { state: post }
-                        );
+                        navigate(`/${post.author.name.replaceAll(" ", "-")}`, {
+                          state: post.author,
+                        });
                       }}
                     >
-                      ......Read More
+                      👤{post.author.name}
                     </div>
-                  </div>
+                  </h3>
+                  <ToastContainer
+                    position="top-center"
+                    autoClose={5000}
+                    hideProgressBar={false}
+                    newestOnTop
+                    closeOnClick
+                    rtl={false}
+                    pauseOnFocusLoss
+                    draggable
+                    pauseOnHover
+                    theme="dark"
+                  />
                 </div>
-                <h3>
-                  <div>📅{post.date}</div>
-                  <div
-                    style={{ cursor: "pointer" }}
+                {/* Scroll-to-top button */}
+                {showScrollToTop && (
+                  <button
+                    className="scrollToTopBtn"
                     onClick={() => {
-                      navigate(`/${post.author.name.replaceAll(" ", "-")}`, {
-                        state: post.author,
-                      });
+                      window.scrollTo({ top: 0, behavior: "smooth" });
                     }}
+                    title="Scroll to top"
                   >
-                    👤{post.author.name}
-                  </div>
-                </h3>
-                <ToastContainer
-                  position="top-center"
-                  autoClose={5000}
-                  hideProgressBar={false}
-                  newestOnTop
-                  closeOnClick
-                  rtl={false}
-                  pauseOnFocusLoss
-                  draggable
-                  pauseOnHover
-                  theme="dark"
-                />
-              </div>
+                    <FontAwesomeIcon icon={faArrowUp} />
+                  </button>
+                )}
+              </>
             );
           })
         ) : (
-            <>
-             
-               <Shimmer />
-               <Shimmer />
-            </>
-     
-        )
-      }
-   
-    </div>
+          <>
+            <Shimmer />
+            <Shimmer />
+          </>
+        )}
+      </div>
+    </>
   );
 }
 
