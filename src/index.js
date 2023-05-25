46f220c1bb7a7dc0b793b38afee86bed2c63a780
Ayushhgupta39 <ayushgupta3902@gmail.com>
2023-05-25 23:27:37 +0530

    #201 Added new create card component

--- src/index.js
+++ src/index.js
@@ -1,6 +1,11 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 import App from './App';
+import '@fontsource/roboto/300.css';
+import '@fontsource/roboto/400.css';
+import '@fontsource/roboto/500.css';
+import '@fontsource/roboto/700.css';
+
 
 const root = ReactDOM.createRoot(document.getElementById('root')); // root
 root.render(
--- src/pages/CreatePost.js
+++ src/pages/CreatePost.js
@@ -1,13 +1,14 @@
 import React, { useState, useEffect } from "react";
 import { addPostToDb } from "../utils/firebase";
 import { useNavigate } from "react-router-dom";
+import { Box, Button, TextField, Typography } from "@mui/material";
+import AddCircleIcon from "@mui/icons-material/AddCircle";
 
 function CreatePost({ isAuth }) {
   const [title, setTitle] = useState("");
   const [postText, setPostText] = useState("");
   const [image, setImage] = useState("");
 
- 
   let navigate = useNavigate();
 
   const createPost = async () => {
@@ -19,7 +20,7 @@ function CreatePost({ isAuth }) {
     if (!isAuth) {
       navigate("/login");
     }
-  // eslint-disable-next-line react-hooks/exhaustive-deps
+    // eslint-disable-next-line react-hooks/exhaustive-deps
   }, []);
 
   const hanldleImageUpload = (e) => {
@@ -27,38 +28,92 @@ function CreatePost({ isAuth }) {
   };
 
   return (
-    <div className="createPostPage">
-      <div className="cpContainer">
-        <h1>Create A Post</h1>
-        <div className="inputGp">
-          <label> Title:</label>
-          <input
-            placeholder="Title..."
+    <Box
+      margin={"2em"}
+      width={"100%"}
+      display={"flex"}
+      justifyContent={"center"}
+    >
+      <Box
+        display={"flex"}
+        flexDirection={"column"}
+        alignItems={"center"}
+        justifyContent={"center"}
+        width={"70%"}
+        bgcolor={"#ffffff"}
+        sx={{ boxShadow: 3, borderRadius: "12px" }}
+        marginLeft={"13em"}
+      >
+        <Typography variant="h1">Create a Post</Typography>
+
+        <Box sx={{ margin: "20px", width: 500, maxWidth: "100%" }}>
+          <TextField
+            fullWidth
+            id="standard-basic"
+            label="Title"
+            variant="standard"
             onChange={(event) => {
               setTitle(event.target.value);
             }}
           />
-        </div>
-        <div className="inputGp">
-          <label> Post:</label>
-          <textarea
-            placeholder="Post..."
+        </Box>
+        <Box
+          sx={{
+            margin: "20px",
+            marginTop: "0",
+            width: 500,
+            maxWidth: "100%",
+          }}
+        >
+          <TextField
+            multiline
+            rows={5}
+            fullWidth
+            id="standard-basic"
+            label="Post"
+            variant="standard"
             onChange={(event) => {
               setPostText(event.target.value);
             }}
           />
-        </div>
-        <div className="inputImg">
-          <label> Image Link</label>
-          <div className="cont">
-            <input placeholder="https://" onChange={hanldleImageUpload} />
+        </Box>
+        <Box
+          display={"flex"}
+          justifyContent={"space-between"}
+          alignItems={"center"}
+          sx={{
+            margin: "20px",
+            marginTop: "0",
+            width: 500,
+            maxWidth: "100%",
+          }}
+        >
+          <TextField
+            id="standard-basic"
+            label="Image Link"
+            variant="outlined"
+            onChange={hanldleImageUpload}
+          />
+          <Box padding={"20px"} sx={{ boxShadow: 1 }}>
             <img src={image} alt="Uploaded preview" />
-          </div>
-        </div>
-
-        <button onClick={createPost}> Submit Post </button>
-      </div>
-    </div>
+          </Box>
+        </Box>
+        <Box
+          marginBottom={"20px"}
+          display={"flex"}
+          justifyContent={"center"}
+          alignItems={"center"}
+        >
+          <Button
+            onClick={createPost}
+            variant="contained"
+            endIcon={<AddCircleIcon />}
+          >
+            Create
+          </Button>
+        </Box>
+      </Box>
+    </Box>
   );
 }
 
