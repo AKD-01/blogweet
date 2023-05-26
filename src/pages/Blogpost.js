7068a078ce2adfaf66075db15d8ef48aec65c701
Sobhan Bera <sobhanbera258@gmail.com>
2023-05-26 12:48:10 +0530

    feat(Blogpost.js): add support for markdown rendering and syntax highlighting This commit adds support for markdown rendering and syntax highlighting in the Blogpost component. The ReactMarkdown library is used to render the markdown text, and the remark-gfm and rehype-raw plugins are used to support GitHub Flavored Markdown. The markdown text is styled using the BlogMarkdownTheme. The markdown text is also displayed with a white-space of pre-line to preserve line breaks. Syntax highlighting is added using the react-syntax-highlighter library. The Blogpost component is also styled using the blogpost.module.scss file.

--- src/pages/Blogpost.js
+++ src/pages/Blogpost.js
@@ -1,9 +1,16 @@
 import { getPostsFromDb } from '../utils/firebase'
 import React, { useEffect, useState } from 'react'
-import { useParams } from 'react-router-dom'
+import { useNavigate, useParams } from 'react-router-dom'
 import { toast, ToastContainer } from 'react-toastify'
-// import "./pages.css";
+import styles from '../styles/pages/blogpost.module.scss'
 import 'react-toastify/dist/ReactToastify.css'
+
+import { ReactMarkdown } from 'react-markdown/lib/react-markdown'
+import remarkGfm from 'remark-gfm'
+import rehypeRaw from 'rehype-raw'
+import { LightMarkdownTheme } from '../theme/BlogMarkdownTheme'
+import markdownStyles from '../styles/markdown.module.scss'
+import SyntaxHighlighter from 'react-syntax-highlighter'
 
 const Blogpost = () => {
   const postId = useParams()
@@ -17,6 +24,7 @@ const Blogpost = () => {
     getPosts()
     // eslint-disable-next-line react-hooks/exhaustive-deps
   }, [])
+  const navigate = useNavigate()
 
   const sharingHandler = s => {
     navigator.clipboard.writeText(`https://blogweet.vercel.app${s}`)
@@ -24,54 +32,163 @@ const Blogpost = () => {
   }
   const postInfo = postLists.filter(x => x.id === postId.blogname)[0]
   console.log(Array.of(postInfo)[0])
-  const post = Array.of(postInfo)[0]
+  // const post = Array.of(postInfo)[0]
+  const post = {
+    title: 'Post to test markdown and post screen',
+    image:
+      'https://images.unsplash.com/photo-1499750310107-5fef28a66643?ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxzZWF',
+    postText: `
+    This is a test post just to test all the markdown syntax:
+    # Header 1
+    ## Header 2
+    ### Header 3
+    
+    One line gap
+    
+    #### Header 4
+    ##### Header 5
+    ##### Header 6
+    
+    > Here is a test blockquote
+    
+    > And here goes the 2nd one
+    
+    \`small code sample\` and here is some random text.
+    
+    * here is the first point
+    * here is another point
+    * here is one more point
+    * and here is the last point
+    
+    [\`link\`](https://github.com/rehypejs/rehype-highlight).
+    
+    Some random paragraphs and **bold text** in between, also why not use ~~italic texts~~ also?
+    
+    ***
+    
+    The code used in this post is here below
+    \`\`\`md
+    This is a test post just to test all the markdown syntax:
+    # Header 1
+    ## Header 2
+    ### Header 3
+    
+    One line gap
+    
+    #### Header 4
+    ##### Header 5
+    ##### Header 6
+    
+    > Here is a test blockquote
+    
+    > And here goes the 2nd one
+    
+    \`small code sample\` and here is some random text.
+    
+    * here is the first point
+    * here is another point
+    * here is one more point
+    * and here is the last point
+    
+    [\`link\`](https://github.com/rehypejs/rehype-highlight).
+    
+    Some random paragraphs and **bold text** in between, also why not use ~~italic texts~~ also?
+    
+    ***
+    \`\`\`
+    `.toString(),
+    author: { name: 'Sobhan Bera' },
+    date: '2023/05/26',
+  }
+
   return (
-    <div className="blogpage">
+    <div className={styles.blogpostRoot}>
       {post && (
-        <>
-          <div className="blogtitle">
-            <div className="three">
-              <h1>{post.title}</h1>
-            </div>
-            <button
-              className="shareButton"
+        <div className={styles.blogContainer}>
+          <div className={styles.blogImage}>
+            <img src={post.image} alt={post.title} />
+          </div>
+
+          <div className={styles.blogTitle}>
+            <h1>{post.title}</h1>
+
+            <i
+              className="bx bxs-share-alt"
               onClick={() =>
                 sharingHandler(
                   `/${post.author.name.replaceAll(' ', '-')}/${post.id}`,
                 )
-              }>
-              <i
-                className="bx bxs-share-alt"
-                style={{
-                  color: 'rgb(255, 255, 255)',
-                  boxShadow: ' 1px 1px 1rem black',
-                  borderRadius: '1rem',
-                  background: ' black',
-                  fontSize: '2rem',
-                  cursor: 'pointer',
-                }}></i>
-            </button>
+              }></i>
           </div>
 
-          <hr />
+          <div className={styles.blogPublisher}>
+            <p
+              className={styles.authorAndDate}
+              style={{ cursor: 'pointer' }}
+              onClick={e => {
+                e.stopPropagation() // will not send click event to parents
 
-          <div className="blogcredits">
-            <div>👤{post.author.name}</div>
-            <div>
-              {post.date != null && (
-                <div style={{ textAlign: 'right', marginRight: '1rem' }}>
-                  📅{post.date}
-                </div>
-              )}
-            </div>
+                navigate(`/${post.author.name.replaceAll(' ', '-')}`, {
+                  state: post.author,
+                })
+              }}>
+              Published by <span>{post.author.name}</span> on 📅 {post.date}
+            </p>
           </div>
-          <hr />
-          <div className="blogContent">
-            <img src={post.image} alt={post.title} />
-            <p>{post.postText}</p>
+
+          <div className={styles.blogContent}>
+            <ReactMarkdown
+              remarkPlugins={[remarkGfm]}
+              rehypePlugins={[rehypeRaw]}
+              className={`${markdownStyles.markdown} ${styles.mardownPostPreview}`}
+              components={{
+                pre({ children }) {
+                  return <>{children}</>
+                },
+                code({ node, inline, className, children, ...props }) {
+                  const match = /language-(\w+)/.exec(className || '')
+
+                  return !inline && match ? (
+                    <SyntaxHighlighter
+                      style={LightMarkdownTheme}
+                      language={match[1]}
+                      PreTag="p"
+                      {...props}>
+                      {String(children).replace(/\n$/, '')}{' '}
+                    </SyntaxHighlighter>
+                  ) : (
+                    <code className={className} {...props}>
+                      {children}
+                    </code>
+                  )
+                },
+
+                p({ children }) {
+                  return <p>{children}</p>
+                },
+
+                strong({ children }) {
+                  return <strong>{children}</strong>
+                },
+
+                a({ href, children }) {
+                  return (
+                    <a href={String(href)} rel="noreferrer" target="_blank">
+                      {children}
+                    </a>
+                  )
+                },
+
+                table({ children }) {
+                  return <table>{children}</table>
+                },
+              }}>
+              {post.postText}
+            </ReactMarkdown>
           </div>
-        </>
+        </div>
       )}
+
       <ToastContainer
         position="top-center"
         autoClose={5000}
