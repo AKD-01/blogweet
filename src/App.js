a4433f52b4ce6e4725d772bb06dd59e2bda74766
Suzy <sulagnaghosh52@gmail.com>
2023-05-28 19:28:42 +0530

    feat: add contributors page

--- src/App.js
+++ src/App.js
@@ -1,30 +1,31 @@
-import "./App.css";
-import { useState } from "react";
-import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
-import {signUserAccountOut} from "./utils/firebase";
-import Home from "./pages/Home";
-import Login from "./pages/Login";
-import CreatePost from "./pages/CreatePost";
-import Sidebar from "./components/SideBar/Sidebar";
-import SidebarOnDesktop from "./components/SidebarOnDesktop/SidebarOnDesktop";
-import Blogpost from "./pages/Blogpost";
-import UserInfo from "./pages/UserInfo";
-import About from "./pages/About";
-import Contact from "./pages/Contact";
-import NotFound404 from "./pages/NotFound404";
+import './App.css'
+import { useState } from 'react'
+import { BrowserRouter as Router, Routes, Route } from 'react-router-dom'
+import { signUserAccountOut } from './utils/firebase'
+import Home from './pages/Home'
+import Login from './pages/Login'
+import CreatePost from './pages/CreatePost'
+import Sidebar from './components/SideBar/Sidebar'
+import SidebarOnDesktop from './components/SidebarOnDesktop/SidebarOnDesktop'
+import Blogpost from './pages/Blogpost'
+import UserInfo from './pages/UserInfo'
+import About from './pages/About'
+import Contact from './pages/Contact'
+import NotFound404 from './pages/NotFound404'
+import Contributors from './pages/Contributors/Contributors'
 
 function App() {
-  const [isAuth, setIsAuth] = useState(localStorage.getItem("isAuth"));
+  const [isAuth, setIsAuth] = useState(localStorage.getItem('isAuth'))
 
   const signUserOut = async () => {
-    await signUserAccountOut();
-    localStorage.clear();
-    setIsAuth(false);
-    window.location.pathname = "/login";
-  };
-  let width = 1000;
+    await signUserAccountOut()
+    localStorage.clear()
+    setIsAuth(false)
+    window.location.pathname = '/login'
+  }
+  let width = 1000
 
-  width = window.screen.width;
+  width = window.screen.width
 
   return (
     <Router>
@@ -33,18 +34,18 @@ function App() {
         <SidebarOnDesktop isAuth={isAuth} signUserOut={signUserOut} />
       )}
       <Routes>
-        <Route path="/" element={<Home isAuth={isAuth} />} />
-        <Route path="/createpost" element={<CreatePost isAuth={isAuth} />} />
-        <Route path="/about" element={<About />} />
-        <Route path="/contact" element={<Contact />} />
-
-        <Route path="/login" element={<Login setIsAuth={setIsAuth} />} />
-        <Route path="/user/:username" element={<UserInfo />} />
-        <Route path="/user/:username/:blogname" element={<Blogpost />} />
-        <Route path="*" element={<NotFound404 />} />
+        <Route path='/' element={<Home isAuth={isAuth} />} />
+        <Route path='/createpost' element={<CreatePost isAuth={isAuth} />} />
+        <Route path='/about' element={<About />} />
+        <Route path='/contact' element={<Contact />} />
+        <Route path='/contributor' element={<Contributors />} />
+        <Route path='/login' element={<Login setIsAuth={setIsAuth} />} />
+        <Route path='/user/:username' element={<UserInfo />} />
+        <Route path='/user/:username/:blogname' element={<Blogpost />} />
+        <Route path='*' element={<NotFound404 />} />
       </Routes>
     </Router>
-  );
+  )
 }
 
-export default App;
+export default App
--- src/components/SidebarOnDesktop/SidebarOnDesktop.js
+++ src/components/SidebarOnDesktop/SidebarOnDesktop.js
@@ -1,80 +1,89 @@
-import React, { useState } from "react";
-import "./SidebarOnDesktop.css";
-import "boxicons";
-import { auth } from "../../utils/firebase";
-import { Link } from "react-router-dom";
+import React, { useState } from 'react'
+import './SidebarOnDesktop.css'
+import 'boxicons'
+import { auth } from '../../utils/firebase'
+import { Link } from 'react-router-dom'
+
 
 const SidebarOnDesktop = ({ isAuth, signUserOut }) => {
-  const [state, setState] = useState(true);
+  const [state, setState] = useState(true)
 
   const toggleSidebarOnDesktop = () => {
-    setState(!state);
-  };
+    setState(!state)
+  }
 
   return (
     <div className={`SidebarOnDesktop active`}>
-      <div className="logo_content">
-        <div className="logo">
+      <div className='logo_content'>
+        <div className='logo'>
           <div
-            className="logoname"
+            className='logoname'
             style={{
-              marginLeft: "5px",
-              fontSize: "1.8rem",
-              marginTop: ".5rem",
+              marginLeft: '5px',
+              fontSize: '1.8rem',
+              marginTop: '.5rem',
             }}
           >
             <b>BLOGWEET</b>
           </div>
         </div>
         <i
-          className="bx bxl-twitter bx-tada"
-          id="btn"
-          style={{ fontSize: "25px" }}
+          className='bx bxl-twitter bx-tada'
+          id='btn'
+          style={{ fontSize: '25px' }}
           onClick={toggleSidebarOnDesktop}
         />
       </div>
-      <ul className="nav_list">
+      <ul className='nav_list'>
         <li onClick={toggleSidebarOnDesktop}>
-          <Link to="/">
-            <i className="bx bxs-home-heart"></i>
-            <span className="link_names">Home</span>
+          <Link to='/'>
+            <i className='bx bxs-home-heart'></i>
+            <span className='link_names'>Home</span>
           </Link>
-          <span className="tooltip">Home</span>
+          <span className='tooltip'>Home</span>
+        </li>
+        
+
+        <li onClick={toggleSidebarOnDesktop}>
+          <Link to='/about'>
+            <i class='bx bxs-info-circle'></i>
+            <span className='link_names'>About</span>
+          </Link>
         </li>
         <li onClick={toggleSidebarOnDesktop}>
-          <Link to="/about">
+          <Link to='/contributor'>
             <i class='bx bxs-info-circle'></i>
-            <span className="link_names">About</span>
+            <span className='link_names'>Contributors</span>
           </Link>
-        </li>   
+        </li>
         <li>
-          <Link to="/contact">
+          <Link to='/contact'>
             <i class='bx bxs-info-circle'></i>
-            <span className="link_names">Contact Us</span>
+            <span className='link_names'>Contact Us</span>
           </Link>
-        </li>  
+        </li>
         {isAuth && (
           <li onClick={toggleSidebarOnDesktop}>
-            <Link to="/createpost">
-              <i className="bx bxs-pencil"></i>
-              <span className="link_names">Create Post</span>
+            <Link to='/createpost'>
+              <i className='bx bxs-pencil'></i>
+              <span className='link_names'>Create Post</span>
             </Link>
-            <span className="tooltip">Create Post</span>
+            <span className='tooltip'>Create Post</span>
           </li>
         )}
         {/* other list items */}
       </ul>
       {!isAuth && (
         <li>
-          <Link to="login" id="log_in">
+          <Link to='login' id='log_in'>
             <i
-              className="bx bx-log-in bx-fade-left"
-              id="log_in"
+              className='bx bx-log-in bx-fade-left'
+              id='log_in'
               onClick={toggleSidebarOnDesktop}
             />
             <span
-              className="link_names"
-              style={{ left: "2.5rem", position: "relative", bottom: ".1rem" }}
+              className='link_names'
+              style={{ left: '2.5rem', position: 'relative', bottom: '.1rem' }}
             >
               Log in
             </span>
@@ -82,24 +91,24 @@ const SidebarOnDesktop = ({ isAuth, signUserOut }) => {
         </li>
       )}
       {isAuth && (
-        <div className="profile_content" id="profile">
-          <div className="profile">
-            <div className="profile_details">
+        <div className='profile_content' id='profile'>
+          <div className='profile'>
+            <div className='profile_details'>
               {auth.currentUser != null && (
-                <img src={auth.currentUser.photoURL} alt="" />
+                <img src={auth.currentUser.photoURL} alt='' />
               )}
-              <div className="name_job">
+              <div className='name_job'>
                 {auth.currentUser != null && (
-                  <div className="name">{auth.currentUser.displayName}</div>
+                  <div className='name'>{auth.currentUser.displayName}</div>
                 )}
               </div>
             </div>
-            <i className="bx bx-log-out" id="log_out" onClick={signUserOut} />
+            <i className='bx bx-log-out' id='log_out' onClick={signUserOut} />
           </div>
         </div>
       )}
     </div>
-  );
-};
+  )
+}
 
-export default SidebarOnDesktop;
+export default SidebarOnDesktop
