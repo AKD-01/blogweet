325445ed5596b9fea64146dbabfdcbf5ca3965a0
Prap1 <81462169+Prap1@users.noreply.github.com>
2023-05-30 14:31:15 +0530

    improve like button

--- src/pages/Blogpost.js
+++ src/pages/Blogpost.js
@@ -11,7 +11,7 @@ import { toast, ToastContainer } from "react-toastify";
 import { db } from "../firebase-config";
 import "./pages.css";
 import "react-toastify/dist/ReactToastify.css";
-import { AiOutlineLike, AiOutlineDislike } from "react-icons/ai";
+import { FaThumbsUp } from "react-icons/fa";
 import MoonLoader from "react-spinners/MoonLoader";
 
 const Blogpost = () => {
@@ -39,29 +39,40 @@ const Blogpost = () => {
     toast(`Your link has been copied to the clipboard. Enjoy!`);
   };
 
-  const toggleLike = async () => {
-    // Update the like status in the database
+const toggleLike = async () => {
+  if (liked) {
+    // Decrease the like count and update the like status in the database
     const postRef = doc(db, "posts", postId.blogname);
-    await updateDoc(postRef, { liked: !liked });
-  
-    // Update the like count locally
+    await updateDoc(postRef, { liked: false, likes: post.likes - 1 });
+
+    // Update the like count and liked state locally
     setPostList((prevPostList) =>
       prevPostList.map((postItem) => {
         if (postItem.id === postId.blogname) {
-          const updatedLikes = liked ? postItem.likes - 1 : postItem.likes + 1;
-          return {
-            ...postItem,
-            liked: !liked,
-            likes: updatedLikes >= 0 ? updatedLikes : 0,
-          };
+          const updatedLikes = postItem.likes - 1 >= 0 ? postItem.likes - 1 : 0;
+          return { ...postItem, liked: false, likes: updatedLikes };
         }
         return postItem;
       })
     );
-  
-    setLiked((prevLiked) => !prevLiked);
-  };
-  
+    setLiked(false);
+  } else {
+    // Update the like status in the database
+    const postRef = doc(db, "posts", postId.blogname);
+    await updateDoc(postRef, { liked: true, likes: post.likes + 1 });
+
+    // Update the like count and liked state locally
+    setPostList((prevPostList) =>
+      prevPostList.map((postItem) => {
+        if (postItem.id === postId.blogname) {
+          return { ...postItem, liked: true, likes: postItem.likes + 1 };
+        }
+        return postItem;
+      })
+    );
+    setLiked(true);
+  }
+};
 
   const getComments = async () => {
     // Retrieve comments from the database based on the postId
@@ -69,35 +80,33 @@ const Blogpost = () => {
       .where("postId", "==", postId.blogname)
       .get();
     const commentsData = commentsQuerySnapshot.docs.map((doc) => doc.data());
-    console.log('commentsData:', commentsData);
+    console.log("commentsData:", commentsData);
     setComments(commentsData);
   };
-
-
 
   const submitComment = async () => {
     // Validate if the newComment is not empty
     if (newComment.trim() === "") {
       return;
     }
-  
+
     // Create a new comment object
     const newCommentObject = {
       postId: postId.blogname,
       comment: newComment,
       timestamp: new Date().getTime(),
     };
-  
+
     // Save the new comment to the database
     await addDoc(collection(db, "comments"), newCommentObject);
-  
+
     // Clear the input field
     setNewComment("");
-  
+
     // Update the comments state with the new comment
     setComments((prevComments) => [...prevComments, newCommentObject]);
   };
-  
+
   useEffect(() => {
     const getComments = async () => {
       // Retrieve comments from the database based on the postId
@@ -107,11 +116,11 @@ const Blogpost = () => {
       const commentsData = commentsQuerySnapshot.docs.map((doc) => doc.data());
       setComments(commentsData);
     };
-  
+
     getComments();
   }, [postId]);
-  console.log('postId.blogname:', postId.blogname);
-getComments();
+  console.log("postId.blogname:", postId.blogname);
+  getComments();
 
   const postInfo = postLists.find((x) => x.id === postId.blogname);
   const post = postInfo ? postInfo : null;
@@ -119,103 +128,103 @@ getComments();
     <div>
       {isLoading ? (
         <div className="centered">
-          <MoonLoader/>
+          <MoonLoader />
         </div>
       ) : (
-    <div className="blogpage">
-      {post && (
-        <>
-          <div className="blogtitle">
-            <div className="three">
-              <h1>{post.title}</h1>
-            </div>
-            <button
-              className="shareButton"
-              onClick={() =>
-                sharingHandler(
-                  `/${post.author.name.replaceAll(" ", "-")}/${post.id}`
-                )
-              }
-            >
-              <i
-                className="bx bxs-share-alt"
-                style={{
-                  color: "rgb(255, 255, 255)",
-                  boxShadow: " 1px 1px 1rem black",
-                  borderRadius: "1rem",
-                  background: "black",
-                  fontSize: "2rem",
-                  cursor: "pointer",
-                }}
-              ></i>
-            </button>
-          </div>
-
-          <hr />
-
-          <div className="blogcredits">
-            <div>👤{post.author.name}</div>
-            {post.date && (
-              <div style={{ textAlign: "right", marginRight: "1rem" }}>
-                📅{post.date}
-              </div>
-            )}
-          </div>
-          <hr />
-          <div className="blogContent">
-            <img src={post.image} alt={post.title} />
-            <p>{post.postText}</p>
-          </div>
-          <div className="likeButton">
-            <button onClick={toggleLike}>
-              {liked ? <AiOutlineDislike /> : <AiOutlineLike />}
-            </button>
-            <span>
-              {post.likes} {post.likes === 1 ? "like" : "likes"}
-            </span>
-          </div>
-          <div className="addComment">
-            <input
-              type="text"
-              value={newComment}
-              onChange={(e) => setNewComment(e.target.value)}
-              placeholder="Add a comment..."
-            />
-            <button onClick={submitComment}>Submit</button>
-          </div>
-          <div className="commentsSection">
-            <h2>Comments</h2>
-            {comments.length > 0 ? (
-              comments.map((comment) => (
-                <div key={comment.timestamp} className="comment">
-                  <p>{comment.comment}</p>
-                  <p className="commentTimestamp">
-                    {new Date(comment.timestamp).toLocaleString()}
-                  </p>
+        <div className="blogpage">
+          {post && (
+            <>
+              <div className="blogtitle">
+                <div className="three">
+                  <h1>{post.title}</h1>
                 </div>
-              ))
-            ) : (
-              <p>No comments yet</p>
-            )}
-          </div>
-        </>
-      )}
-      <ToastContainer
-        position="top-center"
-        autoClose={5000}
-        hideProgressBar={false}
-        newestOnTop
-        closeOnClick
-        rtl={false}
-        pauseOnFocusLoss
-        draggable
-        pauseOnHover
-        theme="dark"
-      />
-    </div>
-      )}
-    </div>
+                <button
+                  className="shareButton"
+                  onClick={() =>
+                    sharingHandler(
+                      `/${post.author.name.replaceAll(" ", "-")}/${post.id}`
+                    )
+                  }
+                >
+                  <i
+                    className="bx bxs-share-alt"
+                    style={{
+                      color: "rgb(255, 255, 255)",
+                      boxShadow: " 1px 1px 1rem black",
+                      borderRadius: "1rem",
+                      background: "black",
+                      fontSize: "2rem",
+                      cursor: "pointer",
+                    }}
+                  ></i>
+                </button>
+              </div>
 
+              <hr />
+
+              <div className="blogcredits">
+                <div>👤{post.author.name}</div>
+                {post.date && (
+                  <div style={{ textAlign: "right", marginRight: "1rem" }}>
+                    📅{post.date}
+                  </div>
+                )}
+              </div>
+              <hr />
+              <div className="blogContent">
+                <img src={post.image} alt={post.title} />
+                <p>{post.postText}</p>
+              </div>
+              <div className="lik">
+                <button className="likeButton" onClick={toggleLike}>
+                  <FaThumbsUp />
+                  
+                </button>
+                <span>
+                  {post.likes} {post.likes === 1 ? "like" : "likes"}
+                </span>
+              </div>
+              <div className="addComment">
+                <input
+                  type="text"
+                  value={newComment}
+                  onChange={(e) => setNewComment(e.target.value)}
+                  placeholder="Add a comment..."
+                />
+                <button onClick={submitComment}>Submit</button>
+              </div>
+              <div className="commentsSection">
+                <h2>Comments</h2>
+                {comments.length > 0 ? (
+                  comments.map((comment) => (
+                    <div key={comment.timestamp} className="comment">
+                      <p>{comment.comment}</p>
+                      <p className="commentTimestamp">
+                        {new Date(comment.timestamp).toLocaleString()}
+                      </p>
+                    </div>
+                  ))
+                ) : (
+                  <p>No comments yet</p>
+                )}
+              </div>
+            </>
+          )}
+          <ToastContainer
+            position="top-center"
+            autoClose={5000}
+            hideProgressBar={false}
+            newestOnTop
+            closeOnClick
+            rtl={false}
+            pauseOnFocusLoss
+            draggable
+            pauseOnHover
+            theme="dark"
+          />
+        </div>
+      )}
+    </div>
   );
 };
 export default Blogpost;
