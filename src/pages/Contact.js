b9eca31bd1d75f7ac9a41d4fdc7dd79955f8b3a9
Priyam Mishra <mishrapriyam2001@gmail.com>
2023-05-31 18:01:15 +0530

    adding emailjs

--- src/pages/Contact.js
+++ src/pages/Contact.js
@@ -1,6 +1,25 @@
 import React from "react";
-import "./Contact.css"
+import "./Contact.css";
+import {useRef} from "react";
+import emailjs from 'emailjs-com';
+
 const Contact=()=>{
+
+    const form = useRef();
+
+    const sendEmail = (e) => {
+      e.preventDefault();
+
+
+    //  YOUR_SERVICE_ID = service_fe7xt7q 
+    //  YOUR_TEMPLATE_ID = template_twttpr5
+    //  YOUR_PUBLIC_KEY = -hZabQ8Vke9nL6Qvs
+  
+      emailjs.sendForm('service_fe7xt7q', 'template_twttpr5', form.current, '-hZabQ8Vke9nL6Qvs')
+        e.target.reset()
+    };
+  
+
     return (
         <div className="Contactus">
             <h1>Contact Us</h1>
@@ -8,7 +27,7 @@ const Contact=()=>{
             <p>
             Please fill out the form below to get in touch with us:
              </p>
-            <form>
+            <form ref={form} onSubmit={sendEmail}>
                 <div >
                 <label htmlFor="name">Name:</label>
                 <input type="text" id="name"></input>
@@ -21,7 +40,7 @@ const Contact=()=>{
                 <label htmlFor="message">Message:</label>
                 <textarea id="message" rows="6"></textarea>
                 </div>
-                <button>Submit</button>
+                <button type='submit'>Submit</button>
             </form>
         </div>
     );
