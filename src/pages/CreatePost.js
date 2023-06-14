b008022d9c25444914df3bb0b4ffe5b9e858febb
shah-kaivan <shahkai999@gmail.com>
2023-06-14 15:46:11 +0530

    modified submit button

--- src/pages/CreatePost.js
+++ src/pages/CreatePost.js
@@ -1,16 +1,28 @@
 import React, { useState, useEffect } from "react";
 import { addPostToDb } from "../utils/firebase";
 import { useNavigate } from "react-router-dom";
+import Modal from "./Modal"; // Import the Modal component
 
 function CreatePost({ isAuth }) {
   const [title, setTitle] = useState("");
   const [postText, setPostText] = useState("");
   const [image, setImage] = useState("");
-
- 
+  const [modalMessage, setModalMessage] = useState(""); // State for the modal message
+  const [showModal, setShowModal] = useState(false); // State to control the modal visibility
   let navigate = useNavigate();
 
   const createPost = async () => {
+    if (!title || !postText || !image) {
+      setModalMessage("Please fill all the fields"); // Set the modal message
+      setShowModal(true); // Show the modal
+      return;
+    }
+    console.log(title, postText, image);
+    if (getWordCount(postText) < 20) {
+      setModalMessage("The post must contain at least 20 words."); // Set the modal message
+      setShowModal(true); // Show the modal
+      return;
+    }
     await addPostToDb(title, postText, image);
     navigate("/");
   };
@@ -19,13 +31,20 @@ function CreatePost({ isAuth }) {
     if (!isAuth) {
       navigate("/login");
     }
-  // eslint-disable-next-line react-hooks/exhaustive-deps
+    // eslint-disable-next-line react-hooks/exhaustive-deps
   }, []);
 
   const hanldleImageUpload = (e) => {
     setImage(e.target.value);
   };
 
+  const getWordCount = (text) => {
+    return text.trim().split(/\s+/).length;
+  };
+
+  const closeModal = () => {
+    setShowModal(false);
+  };
 
   return (
     <div className="createPostPage">
@@ -35,18 +54,16 @@ function CreatePost({ isAuth }) {
           <label> Title:</label>
           <input
             placeholder="Title..."
-            onChange={(event) => {
-              setTitle(event.target.value);
-            }}
+            value={title}
+            onChange={(e) => setTitle(e.target.value)}
           />
         </div>
         <div className="inputGp">
           <label> Post:</label>
           <textarea
             placeholder="Post..."
-            onChange={(event) => {
-              setPostText(event.target.value);
-            }}
+            value={postText}
+            onChange={(e) => setPostText(e.target.value)}
           />
         </div>
         <div className="inputImg">
@@ -56,8 +73,11 @@ function CreatePost({ isAuth }) {
             <img src={image} alt="Uploaded preview" />
           </div>
         </div>
-        <button onClick={createPost} onMouseOver = {(e) => {e.target.style.background = ' #FF6347 ';}} onMouseLeave = {(e) => {e.target.style.background = ' white ';}}> Submit Post </button>
+
+        <button onClick={createPost} onMouseOver = {(e) => {e.target.style.background = '#FF6347';}} onMouseLeave = {(e) => {e.target.style.background = 'white';}}> Submit Post </button>
       </div>
+
+      {showModal && <Modal message={modalMessage} closeModal={closeModal} />}
     </div>
   );
 }
