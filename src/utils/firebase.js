86be17c7efb34c093a10caaf6302c55859d655ca
Once-kaur <95931242+Once-kaur@users.noreply.github.com>
2023-06-15 21:47:37 +0530

    sroll-down-progress-bar

--- src/utils/firebase.js
+++ src/utils/firebase.js
@@ -1,5 +1,6 @@
 // Import the functions you need from the SDKs you need
 import { initializeApp } from "firebase/app";
+import "./a.env.local";
 import {
   getFirestore,
   addDoc,
@@ -17,12 +18,19 @@ import {
 } from "firebase/auth";
 
 const firebaseConfig = {
-  apiKey: process.env.REACT_APP_API_KEY,
-  authDomain: process.env.REACT_APP_AUTH_DOMAIN,
-  projectId: process.env.REACT_APP_PROJECT_ID,
-  storageBucket: process.env.REACT_APP_STORAGE_BUCKET,
-  messagingSenderId: process.env.REACT_APP_MESSAGE_SENDER_ID,
-  appId: process.env.REACT_APP_APP_ID,
+// REACT_APP_API_KEY="AIzaSyD9tMXwqU7ijIKnQu0vsX1UoR9FM_w7r2E"
+// REACT_APP_AUTH_DOMAIN="blog-5814a.firebaseapp.com"
+// REACT_APP_PROJECT_ID="blog-5814a"
+// REACT_APP_STORAGE_BUCKET="blog-5814a.appspot.com"
+// REACT_APP_MESSAGE_SENDER_ID="787500061298"
+// REACT_APP_APP_ID="1:787500061298:web:242f526d6800f0bbbf68eb"
+// GENERATE_SOURCEMAP=false,
+  apiKey:"AIzaSyD9tMXwqU7ijIKnQu0vsX1UoR9FM_w7r2E",
+  authDomain:"blog-5814a.firebaseapp.com",
+  projectId: "blog-5814a",
+  storageBucket:"blog-5814a.appspot.com",
+  messagingSenderId: "787500061298",
+  appId: "1:787500061298:web:242f526d6800f0bbbf68eb"
 };
 
 // Initialize Firebase
