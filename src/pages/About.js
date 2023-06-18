1cf4beaa5dbfb00f05efecfe7a0dc94c4129d8e9
karthik <karthiknadar1204@gmail.com>
2023-06-18 17:02:55 +0530

    About page revamp

--- src/pages/About.js
+++ src/pages/About.js
@@ -1,3 +1,62 @@
+// import React from "react";
+// import "./About.css";
+
+// const About = () => {
+//   return (
+//     <div className="AboutPage">
+//       <h1>ABOUT</h1>
+//       <hr />
+//       <p>
+//         BlogWeet is a social media app that allows users to create, post and
+//         share their blogs. It is a platform that encourages users to express
+//         their thoughts, opinions, and perspectives through written content, and
+//         engage with the content of others. The app is easy to use, and it is
+//         designed to be accessible to users of all ages and levels of technical
+//         expertise. Users can create their blogs and post their content with just
+//         a few clicks. <br />
+//         <br />
+//         Once a blog post is published, other users can read, and share it. This
+//         creates a vibrant and dynamic community of users who are passionate
+//         about sharing and discovering new ideas and perspectives. The platform
+//         is also searchable, so users can easily find content that is relevant to
+//         their interests.
+//         <br />
+//         <br />
+//         One of the unique features of BlogWeet is that it allows users to build
+//         a following by connecting with other users who share similar interests.
+//         This feature enables users to expand their reach, and increase the
+//         visibility of their content. Overall, BlogWeet is a great platform for
+//         users who are looking for an easy, user-friendly way to create, post and
+//         share their blogs, and engage with a community of like-minded
+//         individuals. Whether you're a blogger looking to expand your reach, or a
+//         reader looking for new perspectives, BlogWeet is the perfect platform
+//         for you.
+//         <br />
+//         <br />
+//         In terms of security, BlogWeet provides robust security features to
+//         ensure that users' data is protected. The platform uses google’s
+//         encryption to secure user data and also uses advanced security measures
+//         to protect against hacking and other malicious activities.
+//         <br />
+//         <br />
+//         Overall, BlogWeet is a versatile and feature-rich social media app that
+//         provides users with a platform to create, post, and share their blogs.
+//         With its focus on user-generated content, BlogWeet is a vibrant and
+//         dynamic community where ideas and perspectives are shared and explored.
+//       </p>
+//       <hr />
+//       <div className="social">
+//         <a href="https://github.com/AKD-01/blogweet">
+//           <i class="bx bxl-github"></i>
+//           <span>GitHub</span>
+//         </a>
+//       </div>
+//     </div>
+//   );
+// };
+
+// export default About;
+
 import React from "react";
 import "./About.css";
 
@@ -5,52 +64,27 @@ const About = () => {
   return (
     <div className="AboutPage">
       <h1>ABOUT</h1>
+      <div class="container">
+        {/* <h1 class="about-heading">ABOUT</h1> */}
+      </div>
+
       <hr />
-      <p>
-        BlogWeet is a social media app that allows users to create, post and
-        share their blogs. It is a platform that encourages users to express
-        their thoughts, opinions, and perspectives through written content, and
-        engage with the content of others. The app is easy to use, and it is
-        designed to be accessible to users of all ages and levels of technical
-        expertise. Users can create their blogs and post their content with just
-        a few clicks. <br />
-        <br />
-        Once a blog post is published, other users can read, and share it. This
-        creates a vibrant and dynamic community of users who are passionate
-        about sharing and discovering new ideas and perspectives. The platform
-        is also searchable, so users can easily find content that is relevant to
-        their interests.
-        <br />
-        <br />
-        One of the unique features of BlogWeet is that it allows users to build
-        a following by connecting with other users who share similar interests.
-        This feature enables users to expand their reach, and increase the
-        visibility of their content. Overall, BlogWeet is a great platform for
-        users who are looking for an easy, user-friendly way to create, post and
-        share their blogs, and engage with a community of like-minded
-        individuals. Whether you're a blogger looking to expand your reach, or a
-        reader looking for new perspectives, BlogWeet is the perfect platform
-        for you.
-        <br />
-        <br />
-        In terms of security, BlogWeet provides robust security features to
-        ensure that users' data is protected. The platform uses google’s
-        encryption to secure user data and also uses advanced security measures
-        to protect against hacking and other malicious activities.
-        <br />
-        <br />
-        Overall, BlogWeet is a versatile and feature-rich social media app that
-        provides users with a platform to create, post, and share their blogs.
-        With its focus on user-generated content, BlogWeet is a vibrant and
-        dynamic community where ideas and perspectives are shared and explored.
-      </p>
+      <div className="aboutbg" >
+      <p class="text-lg leading-loose text-gray-800">
+    <strong>Welcome to BlogWeet: Your Platform for Expression!</strong> Share your thoughts, opinions, and perspectives through captivating blog posts and connect with a vibrant community of like-minded individuals. 🌟<br /><br />
+    <strong>Create, Share, Inspire!</strong> Our user-friendly app empowers you to effortlessly unleash your creativity and engage with a diverse range of content. Explore fascinating ideas and be part of the conversation! 💡<br /><br />
+    <strong><u>Connect and Expand Your Reach!</u></strong> Forge meaningful connections with fellow bloggers who share your passions. Grow your audience, gain followers, and amplify your impact. Let your voice resonate across our dynamic network! 🚀<br /><br />
+    <strong>Your Security is Our Priority!</strong> With state-of-the-art encryption and advanced security measures, your data is safeguarded. BlogWeet ensures a safe environment, so you can focus on sharing your stories and ideas with peace of mind. 🔒<br /><br />
+    <strong><em>Join BlogWeet Today and Unleash Your Creativity!</em></strong> Our feature-rich platform provides you with intuitive tools to craft visually stunning blog posts and connect with a diverse and supportive community. Experience the thrill of sharing and exploring unique perspectives! 🌐<br /><br />
+    <strong>BlogWeet: Ignite Your Passion, Inspire the World!</strong> 🌈🔥
+</p>
+
+      </div>
       <hr />
       <div className="social">
-        <a href="https://github.com/AKD-01/blogweet">
-          <i class="bx bxl-github"></i>
-          <span>GitHub</span>
-        </a>
+      <a href="https://github.com/AKD-01/blogweet"><i class='bx bxl-github'></i><span>GitHub</span></a>
       </div>
+
     </div>
   );
 };
