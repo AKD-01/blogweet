38d1f6911c17119777f9445bba9344d6a6117ceb
Anamika Yadav <aanyayadav419@gmail.com>
2023-07-05 20:05:54 +0530

    prettier

--- src/pages/Contact.js
+++ src/pages/Contact.js
@@ -1,43 +1,43 @@
 import React from "react";
-import "./Contact.css"
+import "./Contact.css";
 import { useNavigate } from "react-router-dom";
 
-const Contact=()=>{
-    const navigate = useNavigate();
-    return (
-        <div className="Contactus">
-            <div className="contact-head">
+const Contact = () => {
+  const navigate = useNavigate();
+  return (
+    <div className="Contactus">
+      <div className="contact-head">
         <i
           onClick={() => {
             navigate("/");
           }}
           className="bx bx-arrow-back"
         ></i>
-            <h1>Contact Us</h1>
+        <h1>Contact Us</h1>
       </div>
-            <hr/>
-            <p>
-            Please fill out the form below to get in touch with us:
-             </p>
-            <form>
-                <div >
-                <label htmlFor="name">Name:</label>
-                <input type="text" id="name" placeholder="Name"></input>
-                </div>
-                <div>
-                <label htmlFor="email">Email:</label>
-                <input type="email" id="email" placeholder="Email"></input>
-            </div>
-                <div>
-                <label htmlFor="message">Message:</label>
-                <textarea id="message" rows="6" placeholder="Enter your message here"></textarea>
-                </div>
-                <button className="cbutton">Submit</button>
-            </form>
+      <hr />
+      <p>Please fill out the form below to get in touch with us:</p>
+      <form>
+        <div>
+          <label htmlFor="name">Name:</label>
+          <input type="text" id="name" placeholder="Name"></input>
         </div>
-    );
-
-    
+        <div>
+          <label htmlFor="email">Email:</label>
+          <input type="email" id="email" placeholder="Email"></input>
+        </div>
+        <div>
+          <label htmlFor="message">Message:</label>
+          <textarea
+            id="message"
+            rows="6"
+            placeholder="Enter your message here"
+          ></textarea>
+        </div>
+        <button className="cbutton">Submit</button>
+      </form>
+    </div>
+  );
 };
 
 export default Contact;
